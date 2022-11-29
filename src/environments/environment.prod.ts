9272d357594d02be88acd2b7584bb69f50994221
Henning Kuehl <henning@kuehlonline.de>
2022-11-29 21:30:17 +1300

    initialize firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'nm-memegen',
+    appId: '1:65942369134:web:277931fd34c461baf3077f',
+    storageBucket: 'nm-memegen.appspot.com',
+    locationId: 'europe-west3',
+    apiKey: 'AIzaSyAdkjpXDpNg9vG_PLhOzC99346iz8G6U-M',
+    authDomain: 'nm-memegen.firebaseapp.com',
+    messagingSenderId: '65942369134',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'nm-memegen',
+    appId: '1:65942369134:web:277931fd34c461baf3077f',
+    storageBucket: 'nm-memegen.appspot.com',
+    locationId: 'europe-west3',
+    apiKey: 'AIzaSyAdkjpXDpNg9vG_PLhOzC99346iz8G6U-M',
+    authDomain: 'nm-memegen.firebaseapp.com',
+    messagingSenderId: '65942369134',
+  },
   production: false
 };
 

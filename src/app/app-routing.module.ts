1104551ce15673d2741dc96f132d2a3bd7980d8f
Henning Kuehl <henning@kuehlonline.de>
2022-11-29 22:00:15 +1300

    build basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/create'
+  },
+  {
+    path: '/create',
+    loadChildren: () => import('./memes/features/create/create.module').then(m => m.CreateModule)
+  },
+  {
+    path: '/gallery',
+    loadChildren: () => import('./memes/features/gallery/gallery.module').then(m => m.GalleryModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
